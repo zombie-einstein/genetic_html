bdaddacf26a79b1b98cf0262c903430fd6b375b4
zombie-einstein <jlanglopez@hotmail.com>
2019-10-17 14:16:18 +0100

    NA

--- genetics.py
+++ genetics.py
@@ -3,14 +3,15 @@ from bs4 import BeautifulSoup as BS
 import cssutils as CS
 
 def recMateSoup(a,b,rate):
+    "Recursicve mating algorithm based on DFS"
     if not (hasattr(a,'contents') and hasattr(b,'contents')): return
     d = max(len(a.contents),len(b.contents))
     # Want to compare equal number of elements so append with empty elements
     a.contents = a.contents+[None for i in xrange(d-len(a.contents))]
     b.contents = b.contents+[None for i in xrange(d-len(b.contents))]
     for i in xrange(d):
-        if RD.random() > rate: a.contents[i],b.contents[i] = b.contents[i],a.contents[i]
-        else: recMateSoup(a.contents[i],b.contents[i],rate)
+        if RD.random() < rate: a.contents[i],b.contents[i] = b.contents[i],a.contents[i] # Swap elements
+        else: recMateSoup(a.contents[i],b.contents[i],rate) # Continue down tree
 
 def mutate(a,rate):
     "DFS iterate over all elements of tree, mutate tags and attributes"
@@ -25,7 +26,7 @@ def mutate_style(a,rate):
         styles = CS.parseStyle(a)
         ret = ""
         for j in styles:
-            if RD.random() > rate:
+            if RD.random() < rate:
                 if "color" in j.name: j.value = randomHexColor()
                 elif "px" in j.value: j.value = str(RD.randint(0,1000))+"px"
                 elif "%%" in j.value: j.value = str(RD.randint(0,100))+"%%"
--- html_generator.py
+++ html_generator.py
@@ -1,26 +1,30 @@
-import xml.etree.ElementTree as ET
-import random as rnd
 import genetics as gen
 from bs4 import BeautifulSoup as BS
+from urllib2 import urlopen as URL
 
 testPage = open("Test.html",'w')
 
-htmlA = '''<html><body><h1 style="color:#ff00ff;background-color:#a5e109;width:50%;">Header A</h1><div style="color: #ff0000">DIV 1A<div style="background-color: #1099aa">SUB-DIV 1A</div></div><div style="background-color: #ff0000">DIV 2B</div></body></html>'''
+testURLA = URL("https://en.wikipedia.org/wiki/Physics")
+testURLB = URL("https://en.wikipedia.org/wiki/Cellular_automaton")
 
-htmlB = '''<html><body><h1 style="color: #ffff00">Header B</h1><div style="color: #00f000">DIV 1B</div><div style="background-color: #ff00ff">DIV 2B</div></body></html>'''
+htmlA = '''<html><head><title>Sample "Hello, World" Application</title></head><body bgcolor=white><table border="0" cellpadding="10">#<tr><td><img src="images/springsource.png"></td><td><h1>Sample "Hello, World" Application</h1></td></tr></table><p>This is the home page for the HelloWorld Web application. </p><p>To prove that they work, you can execute either of the following links:<ul><li>To a <a href="hello.jsp">JSP page</a>.<li>To a <a href="hello">servlet</a>.</ul></body></html>'''
 
-soupTestA = BS(htmlA, "lxml")
-soupTestB = BS(htmlB, "lxml")
+htmlB = '''<html><head><title>Example</title></head><body><h1 style="color: #ffff00">Header B</h1><div style="color: #00f000">DIV 1B</div><div style="background-color: #ff00ff">DIV 2B</div></body></html>'''
 
-testPage.write(str(soupTestA))
-testPage.write(str(soupTestB))
-
-for i in xrange(1000):
-    gen.recMateSoup(soupTestA.body,soupTestB.body,0.5)
-    gen.mutate(soupTestA,0.01)
-    gen.mutate(soupTestB,0.01)
-    testPage.write(str(soupTestA)+"\n")
+soupTestA = BS(testURLA, "lxml-xml")
+soupTestB = BS(testURLB, "lxml-xml")
 
 testPage.write(str(soupTestA)+"\n")
+testPage.write(str(soupTestB)+"\n")
+
+for i in xrange(1):
+    gen.recMateSoup(soupTestA,soupTestB,0.5)
+    gen.mutate(soupTestA,0.001)
+    gen.mutate(soupTestB,0.001)
+    #testPage.write(str(soupTestA)+"\n")
+#   testPage.write(str(soupTestB)+"\n")
+#testPage.write("<html><body>--------------------------------------------</body></html>")
+#testPage.write(str(soupTestA)+"\n")
+#testPage.write(str(soupTestB)+"\n")
 
 testPage.close()
